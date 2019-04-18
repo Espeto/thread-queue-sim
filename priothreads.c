44451acef8f06b21452d8b354b95c9e1debf8ab0
Gilberto Kreisler <gkreisler5@hotmail.com>
2019-04-18 07:58:27 -0300

    problema na estrutura das structs

--- priothreads.c
+++ priothreads.c
@@ -3,6 +3,7 @@
 
 /* inclua aqui as bibliotecas que precisar */
 #include <stdlib.h>
+#include <stdio.h>
 
 /* adicione o que for preciso para uma thread ter prioridade e ficar bloqueada esperando sua vez de executar */
 typedef struct {
@@ -14,21 +15,25 @@ typedef struct {
 } pt_thread_ctx;
 
 /* Recomendo declarar tipos e estruturas internas aqui. */
+#define NUM_FILA 8
+
 typedef struct {
 	void *(*funcao) (void *);
 
 } Processor;
 
 typedef struct {
-	pt_thread_ctx *prox;
-}queue_node;
+	pt_thread_ctx thread_pcb;
+	pt_thread_ctx *next;
+} Node;
 
 typedef struct {
-	queue_node *head;
-	queue_node *tail;
-}Queue;
+	Node *head;
+	Node *tail;
+} Queue;
 
-Queue *threads_queue;
+Queue **queues;
+Processor *processors;
 
 /* Reinicializa o escalonador. Descarta todo o estado interno atual (limpa filas) e
    define o número de processadores virtuais entre 1 e 8. Default é 1 processador.
@@ -36,8 +41,56 @@ Queue *threads_queue;
 */
 void pt_init(unsigned int processadores){
 
+	if(processadores > 1 && processadores <= 8) {
+		processors = (Processor*) malloc(processadores * sizeof(Processor));
+	}
+	else {
+		processors = (Processor*) malloc(processadores * sizeof(Processor));
+	}
+
+	if(processors == NULL) {
+		printf("Erro de alocação de memória\n");
+		exit(1);
+	}
 }
 
+/*
+	Inicializa/Reinicializa as filas
+*/
+void init_queue() {
+	Node *aux;
+	Queue *single_queue;
+
+	if (queues == NULL) {
+		queues = (Queue**) malloc(NUM_FILA * sizeof(Queue*));
+		if(queues == NULL) {
+			printf("Erro de alocação de memória\n");
+			exit(1);
+		}
+		for (int i = 0; i < NUM_FILA; i++) {
+			queues[i]->head = NULL;
+			queues[i]->tail = NULL;
+		}
+	}
+
+	else {
+		single_queue = queues[i];
+		aux = single_queue->head;
+
+		do {
+			queues->head = aux->next;
+		}while(aux != NULL);
+	}
+
+
+}
+
+/*
+	Scheduler dos processadores
+*/
+void scheduler() {
+
+}
 
 /* Cria uma nova thread com a prioridade entre 1 e 8. Default é 8. Valores menores
    definem maior prioridade. Caso o usuário defina uma prioridade inválida, use o default.
