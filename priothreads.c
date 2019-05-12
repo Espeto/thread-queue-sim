6cc2f8fdb5dc57df0df5f34fa8773151bfd816ee
Gilberto Kreisler <gkreisler5@hotmail.com>
2019-05-12 01:31:09 -0300

    scheduler dos processadores

--- priothreads.c
+++ priothreads.c
@@ -1,7 +1,7 @@
 #include "priothreads.h"
 #include <pthread.h>
 
-#define MAX_POS 8
+#define MAX_SIZE 8
 
 /* inclua aqui as bibliotecas que precisar */
 #include <stdlib.h>
@@ -21,12 +21,11 @@ typedef struct pt_thread_ctx{
 } pt_thread_ctx;
 
 /* Recomendo declarar tipos e estruturas internas aqui. */
-pt_thread_ctx *all_threads_pointer;
 
-pthread_cond_t queue_cond[MAX_POS];
-pthread_mutex_t threads_per_queue_mutex[MAX_POS];
-int is_empty[MAX_POS];
-int threads_per_queue[MAX_POS];
+pthread_cond_t queue_cond[MAX_SIZE];
+pthread_mutex_t threads_per_queue_mutex[MAX_SIZE];
+int is_empty[MAX_SIZE];
+int threads_per_queue[MAX_SIZE];
 
 unsigned int total_processors;
 
@@ -36,10 +35,10 @@ pthread_cond_t all_processors_inuse;
 pthread_mutex_t inuse_processors_mutex;
 unsigned int inuse_processors_count;
 
-pthread_cond_t zero_threads;
 pthread_mutex_t total_threads_count_mutex;
 unsigned int total_threads_count;
 
+pthread_cond_t zero_threads;
 pthread_mutex_t is_scheduler_sleeping_mutex;
 int is_scheduler_sleeping;
 
@@ -66,7 +65,7 @@ void pt_init(unsigned int processadores){
 	pthread_mutex_init(&total_threads_count_mutex, NULL);
 	pthread_cond_init(&zero_threads, NULL);
 
-	for (size_t i = 0; i < MAX_POS; i++) {
+	for (size_t i = 0; i < MAX_SIZE; i++) {
 		pthread_cond_init(&queue_cond[i], NULL);
 		is_empty[i] = 1;
 		pthread_mutex_init(&threads_per_queue_mutex[i], NULL);
@@ -77,13 +76,11 @@ void pt_init(unsigned int processadores){
 
 }
 
-/*
-	Scheduler dos processadores
+/* Scheduler dos processadores
 	é chamada na inicialização
 */
 void *scheduler() {
 	int status;
-	int i = 0;
 
 	while(1) {
 
@@ -96,13 +93,13 @@ void *scheduler() {
 		// 	return;
 		// }
 
+		//se não houver threads em nenhuma fila, chama wait aguardando por uma
 		pthread_mutex_lock(&total_threads_count_mutex);
-		if(total_threads_count == 0) {
+		//evitar spurious wake-up
+		while(total_threads_count == 0) {
 			pthread_mutex_lock(&is_scheduler_sleeping_mutex);
 			is_scheduler_sleeping = 1;
 			pthread_mutex_unlock(&is_scheduler_sleeping_mutex);
-		}
-		while(total_threads_count == 0) {
 			status = pthread_cond_wait(&zero_threads, &total_threads_count_mutex);
 			//Handle possível erro com a condição
 			if(status != 0) {
@@ -112,17 +109,38 @@ void *scheduler() {
 		}
 		pthread_mutex_unlock(&total_threads_count_mutex);
 
-		while(total_threads_count > 0) {
-			int high_priority_pos = pos_high_prority_thread();
-			if
+		for (size_t i = 0; i < MAX_SIZE; i++) {
+			pthread_mutex_lock(&threads_per_queue_mutex[i]);
+			if(is_empty[i]) {
+					pthread_mutex_unlock(&threads_per_queue_mutex[i]);
+					continue;
+			}
+			//Tem coisa pra ajeitar na lógica desse if
+			if(is_high_prority_thread(i)) {
+				pthread_cond_signal(&queue_cond[i]);
+				threads_per_queue[i]--;
+				if(threads_per_queue[i] == 0) is_empty[i] = 1;
+				pthread_mutex_unlock(&threads_per_queue_mutex[i]);
+				break;
+			}
+			pthread_mutex_unlock(&threads_per_queue_mutex[i]);
 		}
-
 	}
 }
 
-int pos_high_prority_thread() {
+/* Checa se a thread atual é a com maior prioridade
+*/
+int is_high_prority_thread(int atual) {
+	for (size_t i = 0; i < atual; i++) {
+		pthread_mutex_lock(&threads_per_queue_mutex[i]);
+		if(!is_empty[i]) {
+			pthread_mutex_unlock(&threads_per_queue_mutex[i]);
+			return 0;
+		}
+		pthread_mutex_unlock(&threads_per_queue_mutex[i]);
+	}
 
-	return 0;
+	return 1;
 }
 
 /* Cria uma nova thread com a prioridade entre 1 e 8. Default é 8. Valores menores
@@ -159,8 +177,8 @@ void *worker_thread(void *arg) {
 
 	pthread_mutex_lock(&threads_per_queue_mutex[thread_ctx->prioridade]);
 	threads_per_queue[thread_ctx->prioridade]++;
-	is_empty[thread_ctx->prioridade] = 0;
-	pthread_mutex_unlock(&&threads_per_queue_mutex[thread_ctx->prioridade]);
+	if(is_empty[thread_ctx->prioridade]) is_empty[thread_ctx->prioridade] = 0;
+	pthread_mutex_unlock(&threads_per_queue_mutex[thread_ctx->prioridade]);
 
 	pthread_mutex_lock(&waiting_call_lock);
 	while(1) {
@@ -207,7 +225,7 @@ void pt_destroy(){
 	pthread_mutex_destroy(&inuse_processors_mutex);
 	pthread_cond_destroy(&inuse_processors_cond);
 
-	for (size_t i = 0; i < MAX_POS; i++) {
+	for (size_t i = 0; i < MAX_SIZE; i++) {
 		pthread_mutex_destroy(&threads_per_queue_mutex[i]);
 		pthread_cond_destroy(&queue_cond[i]);
 	}
